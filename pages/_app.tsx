0416ba55fb6d7abede3a2b38e210504d5e94c7bd
Harish <hshejokar@gmail.com>
2024-12-12 15:52:29 +0530

    connected sign up button to backend;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from "@react-oauth/google"
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GoogleOAuthProvider clientId="1057379915345-s3q7ehemjt8jpr3b7eub2neajbvrg5t7.apps.googleusercontent.com">
       <Component {...pageProps} />
+      <Toaster />
     </GoogleOAuthProvider>
 
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoHomeSharp } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
@@ -10,73 +10,94 @@ import { FiUsers } from "react-icons/fi";
 import { FaRegUser } from "react-icons/fa6";
 import { CiCircleMore } from "react-icons/ci";
 import FeedCard from "@/components/feedCard";
-import { GoogleLogin } from "@react-oauth/google";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { graphQLClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import toast from "react-hot-toast";
+
 
 
 
 interface twitterSideBarButton {
   title: string,
-  icons : React.ReactNode,
+  icons: React.ReactNode,
 }
 
 const sideBarMenuItem: twitterSideBarButton[] = [
   {
     title: "Home",
-    icons : <IoHomeSharp />
+    icons: <IoHomeSharp />
   },
   {
     title: "Explore",
-    icons : <IoSearch />
+    icons: <IoSearch />
   },
   {
     title: "Notifications",
-    icons : <FaRegBell />
+    icons: <FaRegBell />
   },
   {
     title: "Messages",
-    icons : <FaRegEnvelope />
+    icons: <FaRegEnvelope />
 
   },
   {
     title: "Grok",
-    icons : <MdOutlineCheckBoxOutlineBlank />
+    icons: <MdOutlineCheckBoxOutlineBlank />
   },
   {
     title: "Bookmarks",
-    icons : <FaRegBookmark />
+    icons: <FaRegBookmark />
   },
   {
     title: "Users",
-    icons : <FiUsers />
+    icons: <FiUsers />
   },
   {
     title: "Profile",
-    icons : <FaRegUser />
+    icons: <FaRegUser />
   },
   {
     title: "More",
-    icons : <CiCircleMore />
+    icons: <CiCircleMore />
   },
 ];
 
-// const handleLoginWithGoogle = async ( (cred:CredentialResponse) => {
-  
-// })
+
 
 export default function Home() {
+
+
+  const handleLoginWithGoogle = useCallback(async (cred: CredentialResponse) => {
+    const googleToken = cred.credential;
+    if (!googleToken) {
+      return toast.error("Google token not found..");
+    }
+
+    const { verifyGoogleToken } = await graphQLClient.request(verifyUserGoogleTokenQuery, { token: googleToken });
+    toast.success("Verified Success..");
+    if(verifyGoogleToken) {
+      window.localStorage.setItem("__twitter_token", verifyGoogleToken );
+    }
+
+
+  }, [])
+
+
+
   return (
     <div className="grid grid-cols-12 h-screen">
       {/* ===============Left-bar===============================*/}
       <div className="col-span-3 pl-[200px]">
         <div className=" text-4xl px-5 hover:bg-[#202327] rounded-full w-fit p-[18px]">
-        <FaXTwitter />
+          <FaXTwitter />
         </div>
         {/* left-bar-icon list */}
         <div className="mt-6">
-        <ul className="flex flex-col gap-4">
-          {sideBarMenuItem.map((item) => { return <li key={item.title} className="flex gap-4 hover:bg-[#202327] rounded-full w-fit px-5 py-3"><span className="text-[26px]">{item.icons}</span><span className="font-bold text-xl">{item.title}</span></li>})}
+          <ul className="flex flex-col gap-4">
+            {sideBarMenuItem.map((item) => { return <li key={item.title} className="flex gap-4 hover:bg-[#202327] rounded-full w-fit px-5 py-3"><span className="text-[26px]">{item.icons}</span><span className="font-bold text-xl">{item.title}</span></li> })}
           </ul>
-          </div>
+        </div>
       </div>
       {/* ==================Middle-section========================= */}
       <div className="col-span-6  border-l-[1px] border-r-[1px] border-gray-400">
@@ -84,19 +105,19 @@ export default function Home() {
         <FeedCard />
         <FeedCard />
         <FeedCard />
-        
+
       </div>
-      
+
       {/* ======================right-section===================== */}
       <div className="col-span-3 p-4">
-      <div className=" border p-5 text-center bg-slate-700 rounded-lg ">
-        <h2 className="font-bold">New to Twitter ?</h2>
-        <div className="flex justify-center mt-2">
-        <GoogleLogin onSuccess={(cred) => console.log(cred)}/>
+        <div className=" border p-5 text-center bg-slate-700 rounded-lg ">
+          <h2 className="font-bold">New to Twitter ?</h2>
+          <div className="flex justify-center mt-2">
+            <GoogleLogin onSuccess={handleLoginWithGoogle} />
+          </div>
         </div>
-       </div>
       </div>
-     
+
     </div>
   );
 }
