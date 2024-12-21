492a69326101a58fedbfb4e277410c5d93c6ac9a
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2024-12-22 00:37:47 +0530

    authentication update

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,110 +14,137 @@ import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import { graphQLClient } from "@/clients/api";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
 import toast from "react-hot-toast";
-
-
-
+import { useCurrentUser } from "@/hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
+import Image from "next/image";
 
 interface twitterSideBarButton {
-  title: string,
-  icons: React.ReactNode,
+  title: string;
+  icons: React.ReactNode;
 }
 
 const sideBarMenuItem: twitterSideBarButton[] = [
   {
     title: "Home",
-    icons: <IoHomeSharp />
+    icons: <IoHomeSharp />,
   },
   {
     title: "Explore",
-    icons: <IoSearch />
+    icons: <IoSearch />,
   },
   {
     title: "Notifications",
-    icons: <FaRegBell />
+    icons: <FaRegBell />,
   },
   {
     title: "Messages",
-    icons: <FaRegEnvelope />
-
+    icons: <FaRegEnvelope />,
   },
   {
     title: "Grok",
-    icons: <MdOutlineCheckBoxOutlineBlank />
+    icons: <MdOutlineCheckBoxOutlineBlank />,
   },
   {
     title: "Bookmarks",
-    icons: <FaRegBookmark />
+    icons: <FaRegBookmark />,
   },
   {
     title: "Users",
-    icons: <FiUsers />
+    icons: <FiUsers />,
   },
   {
     title: "Profile",
-    icons: <FaRegUser />
+    icons: <FaRegUser />,
   },
   {
     title: "More",
-    icons: <CiCircleMore />
+    icons: <CiCircleMore />,
   },
 ];
 
-
-
 export default function Home() {
+  const { user } = useCurrentUser();
+  const queryClient = useQueryClient();
+  const handleLoginWithGoogle = useCallback(
+    async (cred: CredentialResponse) => {
+      const googleToken = cred.credential;
+      if (!googleToken) {
+        return toast.error("Google token not found..");
+      }
+      const { verifyGoogleToken } = await graphQLClient.request(
+        verifyUserGoogleTokenQuery,
+        { token: googleToken }
+      );
 
-
-  const handleLoginWithGoogle = useCallback(async (cred: CredentialResponse) => {
-    const googleToken = cred.credential;
-    if (!googleToken) {
-      return toast.error("Google token not found..");
-    }
-
-    const { verifyGoogleToken } = await graphQLClient.request(verifyUserGoogleTokenQuery, { token: googleToken });
-    toast.success("Verified Success..");
-    if(verifyGoogleToken) {
-      window.localStorage.setItem("__twitter_token", verifyGoogleToken );
-    }
-
-
-  }, [])
-
-
+      toast.success("Verified Success..");
+      if (verifyGoogleToken) {
+        window.localStorage.setItem("__twitter_token", verifyGoogleToken);
+        await queryClient.invalidateQueries(["current-user"]);
+      }
+    },
+    [queryClient]
+  );
 
   return (
     <div className="grid grid-cols-12 h-screen">
       {/* ===============Left-bar===============================*/}
-      <div className="col-span-3 pl-[200px]">
+      <div className="col-span-3 pl-[200px] relative">
         <div className=" text-4xl px-5 hover:bg-[#202327] rounded-full w-fit p-[18px]">
           <FaXTwitter />
         </div>
         {/* left-bar-icon list */}
         <div className="mt-6">
           <ul className="flex flex-col gap-4">
-            {sideBarMenuItem.map((item) => { return <li key={item.title} className="flex gap-4 hover:bg-[#202327] rounded-full w-fit px-5 py-3"><span className="text-[26px]">{item.icons}</span><span className="font-bold text-xl">{item.title}</span></li> })}
+            {sideBarMenuItem.map((item) => {
+              return (
+                <li
+                  key={item.title}
+                  className="flex gap-4 hover:bg-[#202327] rounded-full w-fit px-5 py-3"
+                >
+                  <span className="text-[26px]">{item.icons}</span>
+                  <span className="font-bold text-xl">{item.title}</span>
+                </li>
+              );
+            })}
           </ul>
         </div>
+        {/* user-profile card----- */}
+        {user && (
+          <div className="flex items-center gap-x-2 px-5 py-3  rounded-full hover:bg-slate-900 absolute bottom-3 cursor-pointer">
+            <span>
+              <Image
+                className="rounded-full"
+                src={user?.profileImageUrl}
+                alt="user profile ..."
+                width={35}
+                height={35}
+              />
+            </span>
+            <div className="text-sm font-bold">
+              <span>{user.firstName}</span> <span>{user.lastName}</span>
+            </div>
+          </div>
+        )}
       </div>
       {/* ==================Middle-section========================= */}
-      <div className="col-span-6  border-l-[1px] border-r-[1px] border-gray-400">
+      <div className="col-span-6  border-l-[1px] border-r-[1px] border-gray-400 overflow-y-auto ">
         <FeedCard />
         <FeedCard />
         <FeedCard />
         <FeedCard />
-
       </div>
 
       {/* ======================right-section===================== */}
       <div className="col-span-3 p-4">
-        <div className=" border p-5 text-center bg-slate-700 rounded-lg ">
-          <h2 className="font-bold">New to Twitter ?</h2>
-          <div className="flex justify-center mt-2">
-            <GoogleLogin onSuccess={handleLoginWithGoogle} />
+        {!user && (
+          <div className=" border p-5 text-center bg-slate-700 rounded-lg ">
+            <h2 className="font-bold">New to Twitter ?</h2>
+            <div className="flex justify-center mt-2">
+              <GoogleLogin onSuccess={handleLoginWithGoogle} />
+            </div>
           </div>
-        </div>
+        )}
       </div>
-
     </div>
   );
 }
